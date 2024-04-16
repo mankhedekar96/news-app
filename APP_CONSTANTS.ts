a8822dd2551b5bf22571d44131855509ad8e41e1
Manish Khedekar <mankhedekar96@gmail.com>
2024-04-16 16:55:57 +0530

    API key updated

--- APP_CONSTANTS.ts
+++ APP_CONSTANTS.ts
@@ -1,5 +1,5 @@
 export const NEWS_URL = "https://newsapi.org/v2/top-headlines";
-export const NEWS_API_KEY = "5484ae71954a4213b436765cacd810f9";
+export const NEWS_API_KEY = "da57527fac87485dbe4c4afaa9ead06d";
 export const NEWS_COUNTRIES = [
     "ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "in", "ua", "us", "ve", "za"
 ];
